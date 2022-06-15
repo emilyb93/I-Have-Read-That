3f3e5b8d70faeb919fa822b02a4026302d7fac3d
Emily Bennett <emily.bennett@northcoders.com>
2022-06-15 20:54:59 +0100

    creating voting elements

--- src/components/ArticleVote/ArticleVote.jsx
+++ src/components/ArticleVote/ArticleVote.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import upvoteEmpty from "../../assets/upvote-empty.png";
+import upvotefull from "../../assets/upvote-full.png";
+import downvoteEmpty from "../../assets/downvote-empty.png";
+import downvoteFull from "../../assets/downvote-full.png";
+import { style } from "./ArticleVote.style.js";
 
-export default function ArticleVote() {
-  return <></>;
+export default function ArticleVote({ article_id, articleVotes }) {
+  const [votes, setVotes] = useState(articleVotes);
+  const [voteChange, setVoteChange] = useState(0);
+
+  return (
+    <>
+      <button>
+        <img src={upvoteEmpty} style={style.upvote} alt="up vote" />
+      </button>
+      <button>
+        <img src={downvoteEmpty} style={style.downvote} alt="down vote" />
+      </button>
+      <p>{votes}</p>
+    </>
+  );
 }
