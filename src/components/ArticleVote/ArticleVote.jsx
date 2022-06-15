47e6e27ff29cdec60071881f0fdf17d264f7f86f
Emily Bennett <emily.bennett@northcoders.com>
2022-06-15 21:48:48 +0100

    adding api call for voting on an article

--- src/components/ArticleVote/ArticleVote.jsx
+++ src/components/ArticleVote/ArticleVote.jsx
@@ -1,23 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import upvoteEmpty from "../../assets/upvote-empty.png";
 import upvoteFull from "../../assets/upvote-full.png";
 import downvoteEmpty from "../../assets/downvote-empty.png";
 import downvoteFull from "../../assets/downvote-full.png";
 import { style } from "./ArticleVote.style.js";
+import { patchArticleVotes } from "../../api";
 
 export default function ArticleVote({ article_id, articleVotes }) {
-  const [votes, setVotes] = useState(articleVotes);
+  const [votes] = useState(articleVotes);
   const [voteChange, setVoteChange] = useState(0);
 
   function handleVote(incVote) {
-    const increaseVote = () => {
+    const oppositeVoteValue = incVote > 0 ? -1 : 1;
+    const changeVote = () => {
+      // This allow the user to only either add or remove a single vote in either direction
       if (voteChange !== incVote) {
         setVoteChange(incVote);
+        patchArticleVotes(article_id, incVote);
       } else {
         setVoteChange(0);
+        patchArticleVotes(article_id, oppositeVoteValue);
       }
     };
-    return increaseVote;
+
+    return changeVote;
   }
 
   return (
