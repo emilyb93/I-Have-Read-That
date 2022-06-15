69f1b71d134c458ca3b8e8875dd0bef3d3c7e147
Emily Bennett <emily.bennett@northcoders.com>
2022-06-15 21:13:28 +0100

    votes optimistically rendering

--- src/components/ArticleVote/ArticleVote.jsx
+++ src/components/ArticleVote/ArticleVote.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import upvoteEmpty from "../../assets/upvote-empty.png";
-import upvotefull from "../../assets/upvote-full.png";
+import upvoteFull from "../../assets/upvote-full.png";
 import downvoteEmpty from "../../assets/downvote-empty.png";
 import downvoteFull from "../../assets/downvote-full.png";
 import { style } from "./ArticleVote.style.js";
@@ -9,15 +9,34 @@ export default function ArticleVote({ article_id, articleVotes }) {
   const [votes, setVotes] = useState(articleVotes);
   const [voteChange, setVoteChange] = useState(0);
 
+  function handleVote(incVote) {
+    const increaseVote = () => {
+      if (voteChange !== incVote) {
+        setVoteChange(incVote);
+      } else {
+        setVoteChange(0);
+      }
+    };
+    return increaseVote;
+  }
+
   return (
     <>
-      <button>
-        <img src={upvoteEmpty} style={style.upvote} alt="up vote" />
+      <button style={style.upvote} onClick={handleVote(1)}>
+        <img
+          src={voteChange > 0 ? upvoteFull : upvoteEmpty}
+          style={style.upvote}
+          alt="up vote"
+        />
       </button>
-      <button>
-        <img src={downvoteEmpty} style={style.downvote} alt="down vote" />
+      <button style={style.downvote} onClick={handleVote(-1)}>
+        <img
+          src={voteChange < 0 ? downvoteFull : downvoteEmpty}
+          style={style.downvote}
+          alt="down vote"
+        />
       </button>
-      <p>{votes}</p>
+      <p>{votes + voteChange}</p>
     </>
   );
 }
