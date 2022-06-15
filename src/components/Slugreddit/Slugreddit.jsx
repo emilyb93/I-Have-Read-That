a736b9a3dcf8c800c8f8b83acc644cfe0bc33d93
Emily Bennett <emily.bennett@northcoders.com>
2022-06-15 11:51:11 +0100

    adding loading and error handling to other components

--- src/components/Slugreddit/Slugreddit.jsx
+++ src/components/Slugreddit/Slugreddit.jsx
@@ -6,10 +6,20 @@ import ArticleList from "../ArticleList.jsx/ArticleList";
 function Slugreddit({ setInfo, topics }) {
   const [articles, setArticles] = useState([]);
   const { slug } = useParams();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
-    fetchArticles(slug).then((fetchedArticles) => {
-      setArticles(fetchedArticles);
-    });
+    fetchArticles(slug)
+      .then((fetchedArticles) => {
+        setArticles(fetchedArticles);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setError(true);
+        setIsLoading(false);
+      });
   }, [slug]);
 
   useEffect(() => {
@@ -25,6 +35,9 @@ function Slugreddit({ setInfo, topics }) {
       });
     }
   }, [slug, topics]);
+
+  if (isLoading) return <p>... Loading ...</p>;
+  if (error) return <p>Somethings gone wrong there sorry</p>;
 
   return <ArticleList articles={articles} />;
 }
