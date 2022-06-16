bcddf50c0680c0925dbb8e392c8517f0811e9744
Emily Bennett <emily.bennett@northcoders.com>
2022-06-16 18:59:48 +0100

    created comment post form

--- src/components/CommentPost/CommentPost.jsx
+++ src/components/CommentPost/CommentPost.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function CommentPost() {
-  return <div>CommentPost</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="comment input">Comment as user</label>
+        <textarea name="comment input"></textarea>
+        <button type="submit">Comment</button>
+      </form>
+    </section>
+  );
 }
 
 export default CommentPost;
