9e1f86724333125591ad07e10f4b9fbe73ca2a30
Emily Bennett <emily.bennett@northcoders.com>
2022-06-16 20:26:28 +0100

    error handling if the post call failed

--- src/components/CommentPost/CommentPost.jsx
+++ src/components/CommentPost/CommentPost.jsx
@@ -4,14 +4,20 @@ import { postComment } from "../../api";
 function CommentPost({ article_id, setComments }) {
   const username = "jessjelly";
   const [body, setBody] = useState("");
+  const [commentError, setCommentError] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    postComment(article_id, username, body).then((newComment) => {
-      setComments((currComments) => {
-        return [newComment, ...currComments];
+    postComment(article_id, username, body)
+      .then((newComment) => {
+        setComments((currComments) => {
+          return [newComment, ...currComments];
+        });
+        setCommentError(false);
+      })
+      .catch(() => {
+        setCommentError(true);
       });
-    });
   };
 
   const handleBodyChange = (e) => {
@@ -28,6 +34,7 @@ function CommentPost({ article_id, setComments }) {
           name="comment input"
         ></textarea>
         <button type="submit">Comment</button>
+        {commentError && <p>Oops, sorry something went wrong there!</p>}
       </form>
     </section>
   );
