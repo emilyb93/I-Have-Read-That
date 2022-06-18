4d9c24d8a003ad8b698a9493f7a7dce9d1e9a70f
Emily Bennett <emily.bennett@northcoders.com>
2022-06-18 14:23:40 +0100

    replacing comment with text to tell the user the comment is deleted

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CommentVote from "../CommentVote/CommentVote";
 import { styles } from "./CommentCard.style";
 import { deleteComment } from "../../api";
@@ -7,34 +7,38 @@ function CommentCard({ comment, setComments }) {
   const { comment_id, body, author, votes } = comment;
 
   const [isDeleting, setIsDeleting] = useState(false);
+  const [error, setError] = useState(false);
+  const [deleted, setDeleted] = useState(false);
+
   const handleDelete = () => {
     setIsDeleting(true);
+    setError(false);
     deleteComment(comment_id)
       .then((res) => {
         if (res.status === 204) {
-          setComments((currentComments) => {
-            const filteredComments = currentComments.filter(
-              (currentComment) => {
-                return currentComment.comment_id !== comment_id;
-              }
-            );
-            return filteredComments;
-          });
           setIsDeleting(false);
+          setDeleted(true);
         }
       })
       .catch(() => {
         setIsDeleting(true);
+        setError(true);
       });
   };
   return (
     <section style={styles.commentCard}>
-      <p>{body}</p>
-      <p>{author}</p>
-      <CommentVote comment_id={comment_id} comment_votes={votes} />
-      <button onClick={handleDelete} disabled={isDeleting}>
-        Delete
-      </button>
+      {deleted ? (
+        <p>Comment deleted by user · just now</p>
+      ) : (
+        <>
+          <p>{body}</p>
+          <p>{author}</p>
+          <CommentVote comment_id={comment_id} comment_votes={votes} />
+          <button onClick={handleDelete} disabled={isDeleting}>
+            Delete
+          </button>
+        </>
+      )}
     </section>
   );
 }
