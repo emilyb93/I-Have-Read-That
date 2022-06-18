ce145b24855648a151fa2ac62e7b21ad841c8d2c
Emily Bennett <emily.bennett@northcoders.com>
2022-06-18 20:56:34 +0100

    extracting topic drop down to component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,33 +3,26 @@ import { Link, NavLink } from "react-router-dom";
 import SluggedIcon from "../../assets/Slugged.png";
 import { useContext } from "react";
 import InfoContext from "../../contexts/InfoContext";
+import TopicDropdown from "../TopicDropdown/TopicDropdown";
 
 const styles = {
   headerContainer: {
     width: "100%",
     height: "4rem",
-    border: "1px solid black",
+    // border: "1px solid black",
     display: "flex",
     flexAlign: "center",
   },
   SluggedIcon: {
-    border: "1px solid black",
+    // border: "1px solid black",
     height: "4rem",
     width: "12rem",
     objectFit: "cover",
   },
-  topicSelector: {
-    width: "12rem",
-  },
-  navigation: {
-    width: "100%",
-    height: "4rem",
-    fontSize: "2rem",
-  },
 };
 
 export default function Header({ topics }) {
-  const [navMenuOpen, setNavMenuOpen] = useState(false);
+  //   const [navMenuOpen, setNavMenuOpen] = useState(false);
   const { info } = useContext(InfoContext);
 
   return (
@@ -37,20 +30,7 @@ export default function Header({ topics }) {
       <Link to="/">
         <img style={styles.SluggedIcon} src={SluggedIcon}></img>
       </Link>
-      <section style={styles.topicSelector}>
-        <nav>
-          <select value={info.slug} style={styles.navigation}>
-            <option>Home</option>
-            {topics.map((topic) => {
-              return (
-                <option key={`topic-${topic.slug}`} to={`/slug/${topic.slug}`}>
-                  {topic.slug}
-                </option>
-              );
-            })}
-          </select>
-        </nav>
-      </section>
+      <TopicDropdown topics={topics} info={info} />
     </header>
   );
 }
