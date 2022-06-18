d4db78fd8febb511907b57ea81e3afbf4002e55b
Emily Bennett <emily.bennett@northcoders.com>
2022-06-18 13:08:38 +0100

    adding delete button

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import CommentVote from "../CommentVote/CommentVote";
+import { styles } from "./CommentCard.style";
 
 function CommentCard({ comment }) {
+  const deleteComment = () => {};
   return (
-    <section>
+    <section style={styles.commentCard}>
       <p>{comment.body}</p>
       <p>{comment.author}</p>
       <CommentVote
         comment_id={comment.comment_id}
         comment_votes={comment.votes}
       />
+      <button onClick={deleteComment}>Delete</button>
     </section>
   );
 }
