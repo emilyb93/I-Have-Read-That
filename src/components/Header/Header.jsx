f451f7edcf756a4a9b8e4e8eb457a6118bdffbf5
Emily Bennett <emily.bennett@northcoders.com>
2022-06-18 18:49:26 +0100

    some styling on the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import SluggedIcon from "../../assets/Slugged.png";
+import { useContext } from "react";
+import InfoContext from "../../contexts/InfoContext";
+
+const styles = {
+  headerContainer: {
+    width: "100%",
+    height: "4rem",
+    border: "1px solid black",
+    display: "flex",
+    flexAlign: "center",
+  },
+  SluggedIcon: {
+    border: "1px solid black",
+    height: "4rem",
+    width: "12rem",
+    objectFit: "cover",
+  },
+  topicSelector: {
+    width: "12rem",
+  },
+  navigation: {
+    width: "100%",
+    height: "4rem",
+    fontSize: "2rem",
+  },
+};
 
 export default function Header({ topics }) {
+  const [navMenuOpen, setNavMenuOpen] = useState(false);
+  const { info } = useContext(InfoContext);
+
   return (
-    <header className="App-header">
+    <header style={styles.headerContainer}>
       <Link to="/">
-        <h1>Slugged</h1>
+        <img style={styles.SluggedIcon} src={SluggedIcon}></img>
       </Link>
-      <nav>
-        {topics.map((topic) => {
-          return (
-            <NavLink key={`topic-${topic.slug}`} to={`/slug/${topic.slug}`}>
-              {topic.slug}
-            </NavLink>
-          );
-        })}
-      </nav>
+      <section style={styles.topicSelector}>
+        <nav>
+          <select value={info.slug} style={styles.navigation}>
+            <option>Home</option>
+            {topics.map((topic) => {
+              return (
+                <option key={`topic-${topic.slug}`} to={`/slug/${topic.slug}`}>
+                  {topic.slug}
+                </option>
+              );
+            })}
+          </select>
+        </nav>
+      </section>
     </header>
   );
 }
