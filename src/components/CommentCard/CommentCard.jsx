e5564308fdeb9202ef3ebcd6b252d6e6c03e482e
Emily Bennett <emily.bennett@northcoders.com>
2022-06-18 14:01:14 +0100

    delete button is disabled while the call is being made

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CommentVote from "../CommentVote/CommentVote";
 import { styles } from "./CommentCard.style";
 import { deleteComment } from "../../api";
@@ -6,24 +6,35 @@ import { deleteComment } from "../../api";
 function CommentCard({ comment, setComments }) {
   const { comment_id, body, author, votes } = comment;
 
+  const [isDeleting, setIsDeleting] = useState(false);
   const handleDelete = () => {
-    deleteComment(comment_id).then((res) => {
-      if (res.status === 204) {
-        setComments((currentComments) => {
-          const filteredComments = currentComments.filter((currentComment) => {
-            return currentComment.comment_id !== comment_id;
+    setIsDeleting(true);
+    deleteComment(comment_id)
+      .then((res) => {
+        if (res.status === 204) {
+          setComments((currentComments) => {
+            const filteredComments = currentComments.filter(
+              (currentComment) => {
+                return currentComment.comment_id !== comment_id;
+              }
+            );
+            return filteredComments;
           });
-          return filteredComments;
-        });
-      }
-    });
+          setIsDeleting(false);
+        }
+      })
+      .catch(() => {
+        setIsDeleting(true);
+      });
   };
   return (
     <section style={styles.commentCard}>
       <p>{body}</p>
       <p>{author}</p>
       <CommentVote comment_id={comment_id} comment_votes={votes} />
-      <button onClick={handleDelete}>Delete</button>
+      <button onClick={handleDelete} disabled={isDeleting}>
+        Delete
+      </button>
     </section>
   );
 }
