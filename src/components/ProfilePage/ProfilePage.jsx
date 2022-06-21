40ae1801f7fb65f4da5ade255f5559512668345d
Emily Bennett <emily.bennett@northcoders.com>
2022-06-21 10:42:22 +0100

    rendering users posts

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { fetchUserPosts } from "../../api";
+import ArticleList from "../ArticleList.jsx/ArticleList";
 
 export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+  const username = "jessjelly";
+  const [userPosts, setUserPosts] = useState([]);
+
+  useEffect(() => {
+    fetchUserPosts("jessjelly").then((posts) => {
+      setUserPosts(posts);
+    });
+  }, []);
+  return (
+    <section>
+      <h2>{username}'s Posts</h2>
+      <ArticleList articles={userPosts} />
+    </section>
+  );
 }
